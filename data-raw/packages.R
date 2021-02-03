6c37723557e25651686cbdd6aec9116da503994d
NicolasJBM <31648860+NicolasJBM@users.noreply.github.com>
2021-02-03 08:31:59 +0100

    chores: update package versions

--- data-raw/packages.R
+++ data-raw/packages.R
@@ -1,3 +1,10 @@
+library(tidyverse)
 
-dat_packages <- read.csv("data-raw/packages.csv")
+versions <- as.data.frame(installed.packages()) %>%
+  dplyr::select(package = Package, version = Version)
+
+dat_packages <- read_csv("data-raw/packages.csv") %>%
+  dplyr::select(-version) %>%
+  dplyr::left_join(versions, by = "package")
+
 save(dat_packages, file = "data/dat_packages.RData")
